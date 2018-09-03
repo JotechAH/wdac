af345edf85d3e6814898ab63f75f5ee0f6e35c24
ImTiara <39366953+ImTiara@users.noreply.github.com>
2018-09-03 16:16:05 +0200

    Update metainf.lua

--- wdac/lua/wdac/metainf.lua
+++ wdac/lua/wdac/metainf.lua
@@ -1,6 +1,6 @@
 if SERVER then
     
-    WDAC.Version = 180518
+    WDAC.Version = 180903
     WDAC.SuspendMessage = "Connection Refused - WatchDog Anti-Cheat\n\nYou cannot connect to the selected server, because it is\nrunning in WDAC(WatchDog Anti-Cheat) secure mode.\n\nThis Steam account has been banned from secure\nserver due to a cheating infraction."
 
 end
